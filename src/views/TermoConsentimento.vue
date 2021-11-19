66293e9f51f77615b5b1487b0e16d9a71873339f
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-19 19:32:49 -0300

    feat: criação tela TermoConsentimento

---
+++ src/views/TermoConsentimento.vue
@@ -0,0 +1,4 @@
+<template>
+    <h1>Termo de Consentimento</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda natus officiis quo similique rem magnam fugiat deleniti? Obcaecati expedita accusantium, aut veniam magnam voluptates iure nobis distinctio asperiores incidunt? Laudantium.</p>
+</template>
