e9b3523304e606ecec903dcdd21da07100d20522
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-19 19:53:31 -0300

    feat: importação fonte via Montserrat

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,19 @@
 </template>
 
 <script>
-export default {
-    methods: {
-        goBack() {
-            window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
-        }
-    }
-}
+  export default {
+      methods: {
+          goBack() {
+              window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+          }
+      }
+  }
 </script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+  * {
+    font-family: 'Montserrat', sans-serif;
+  }
+</style>
