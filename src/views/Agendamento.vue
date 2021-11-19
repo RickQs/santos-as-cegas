20018c6a83ccc348ba0193416e230db38cb9a14b
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-19 19:31:59 -0300

    feat: estruturação tela Agendamento

--- src/views/Agendamento.vue
+++ src/views/Agendamento.vue
@@ -1,3 +1,31 @@
 <template>
-    <h1>Teste</h1>
+    <h1>Agendamento</h1>
+    <h2>Selecione a data e horário.</h2>
+    <h3>Terças-feiras</h3>
+    <div class="area">
+        <router-link to="views/cadastro" class="card"></router-link>
+        <router-link to="views/cadastro" class="card"></router-link>
+        <router-link to="views/cadastro" class="card"></router-link>
+        <router-link to="views/cadastro" class="card"></router-link>
+    </div>
+    <h3>Quartas-feiras</h3>
+    <div class="area">
+        <router-link to="views/cadastro" class="card"></router-link>
+        <router-link to="views/cadastro" class="card"></router-link>
+        <router-link to="views/cadastro" class="card"></router-link>
+        <router-link to="views/cadastro" class="card"></router-link>
+    </div>
 </template>
+
+<style scoped>
+    .area {
+        display: flex;
+        justify-content: center;
+        gap: 5px;
+    }
+    .card {
+        width: 200px;
+        height: 200px;
+        background-color: purple;
+    }
+</style>
