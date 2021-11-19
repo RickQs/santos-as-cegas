4d2b3214fb2d18cf21d73d84cfe8e646526518f3
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-19 19:30:41 -0300

    feat: adição novas rotas

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 import Agendamento from '../views/Agendamento.vue'
+import Cadastro from '../views/Cadastro.vue'
+import TermoConsentimento from '../views/TermoConsentimento.vue'
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
   {
     path: '/agendamento',
     name: 'Agendamento',
     component: Agendamento
+  },
+  {
+    path: '/cadastro',
+    name: 'Cadastro',
+    component: Cadastro
+  },
+  {
+    path: '/termo-consentimento',
+    name: 'TermoConsentimento',
+    component: TermoConsentimento
   }
 ]
 
