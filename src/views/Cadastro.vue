ecb716dc97f20255fa9407120ba64b4841a24168
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-19 19:32:17 -0300

    feat: criação e estruturação tela Cadastro

---
+++ src/views/Cadastro.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Cadastro</h1>
+    <h2>Preencha o formulário com suas informações.</h2>
+    <form>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <router-link to="views/termo-consentimento">
+            <button>Continuar</button>
+        </router-link>
+    </form>
+</template>
+
+<style scoped>
+    form {
+        display: flex;
+        flex-direction: column;
+        row-gap: 10px;
+    }
+
+    button {
+        width: 100%;
+    }
+</style>
