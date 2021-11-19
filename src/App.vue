b4276c42c4c54c5f5685b3b10f03ae33427ca377
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-19 19:30:18 -0300

    feat: criação metódo goBack()

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
 <template>
+  <button @click="goBack">Voltar</button>
   <router-view/>
 </template>
+
+<script>
+export default {
+    methods: {
+        goBack() {
+            window.history.length > 1 ? this.$router.go(-1) : this.$router.push('/')
+        }
+    }
+}
+</script>
