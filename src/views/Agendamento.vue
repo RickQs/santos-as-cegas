850e74737a20ecbc9877816e1a416366a028a27f
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-20 13:54:03 -0300

    refactor: melhoria Accordion dinâmico

--- src/views/Agendamento.vue
+++ src/views/Agendamento.vue
@@ -3,42 +3,22 @@
     <h2>Selecione a data e horário.</h2>
 
     <section>
-        <div class="accordion" id="accordionPanelsStayOpenExample">
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                        Terças-feiras
+        <div class="accordion" id="accordionPanelsStayOpen">
+            <div v-for="(date, weekday, i) in dates" :key="i" class="accordion-item">
+                <h2 class="accordion-header" :id="`panelsStayOpen-heading${capitalizeString(weekday)}`">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" :data-bs-target="`#panelsStayOpen-collapse${capitalizeString(weekday)}`" aria-expanded="true" :aria-controls="`panelsStayOpen-collapse${capitalizeString(weekday)}`">
+                        {{ weekdays[i] }}
                     </button>
                 </h2>
-                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                <div :id="`panelsStayOpen-collapse${capitalizeString(weekday)}`" class="accordion-collapse collapse show" :aria-labelledby="`panelsStayOpen-heading${capitalizeString(weekday)}`">
                     <div class="accordion-body">
                         <div class="d-flex justify-content-evenly flex-wrap">
-                            <router-link v-for="date in dates[0].tuesday" :key="date.id" to="views/cadastro" class="btn btn-light border-success">
+                            <router-link v-for="items in date" :key="items.id" to="views/cadastro" class="btn btn-light border-success">
                                 <span>
-                                    <strong>{{ date.date }}</strong>
+                                    <strong>{{ items.date }}</strong>
                                 </span>
                                 <br />
-                                <span>{{ date.hour }}</span>
-                            </router-link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="accordion-item">
-                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
-                        Quartas-feiras
-                    </button>
-                </h2>
-                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
-                    <div class="accordion-body">
-                        <div class="d-flex justify-content-evenly flex-wrap">
-                            <router-link v-for="date in dates[0].wednesday" :key="date.id" to="views/cadastro" class="btn btn-light border-success">
-                                <span>
-                                    <strong>{{ date.date }}</strong>
-                                </span>
-                                <br />
-                                <span>{{ date.hour }}</span>
+                                <span>{{ items.hour }}</span>
                             </router-link>
                         </div>
                     </div>
@@ -54,12 +34,18 @@
     export default {
         data() {
             return {
-                dates
+                dates: dates[0],
+                weekdays: ['Terças-feiras', 'Quartas-feiras']
+            }
+        },
+        methods: {
+            capitalizeString(string) {
+                return string.charAt(0).toUpperCase() + string.slice(1)
             }
         }
     }
 </script>
 
 <style scoped>
-
+    
 </style>
