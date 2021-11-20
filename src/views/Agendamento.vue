c9c1512d58b71d97aa76b686b481ed1e011824e9
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-20 13:05:20 -0300

    refactor: Accordion dinâmico

--- src/views/Agendamento.vue
+++ src/views/Agendamento.vue
@@ -2,52 +2,64 @@
     <h1>Agendamento</h1>
     <h2>Selecione a data e horário.</h2>
 
-    <div class="accordion" id="accordionPanelsStayOpenExample">
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    Terças-feiras
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                <div class="accordion-body">
-                    <div class="d-flex justify-content-evenly">
-                        <router-link v-for="(card, i) in cards.slice(0,2)" :key="i" to="views/cadastro" class="card btn btn-secondary"></router-link>
+    <section>
+        <div class="accordion" id="accordionPanelsStayOpenExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                        Terças-feiras
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                    <div class="accordion-body">
+                        <div class="d-flex justify-content-evenly flex-wrap">
+                            <router-link v-for="date in dates[0].tuesday" :key="date.id" to="views/cadastro" class="btn btn-light border-success">
+                                <span>
+                                    <strong>{{ date.date }}</strong>
+                                </span>
+                                <br />
+                                <span>{{ date.hour }}</span>
+                            </router-link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
+                        Quartas-feiras
+                    </button>
+                </h2>
+                <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
+                    <div class="accordion-body">
+                        <div class="d-flex justify-content-evenly flex-wrap">
+                            <router-link v-for="date in dates[0].wednesday" :key="date.id" to="views/cadastro" class="btn btn-light border-success">
+                                <span>
+                                    <strong>{{ date.date }}</strong>
+                                </span>
+                                <br />
+                                <span>{{ date.hour }}</span>
+                            </router-link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="accordion-item">
-            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="true" aria-controls="panelsStayOpen-collapseTwo">
-                    Quartas-feiras
-                </button>
-            </h2>
-            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingTwo">
-                <div class="accordion-body">
-                    <div class="d-flex justify-content-evenly">
-                        <router-link v-for="(card, i) in cards" :key="i" to="views/cadastro" class="card btn btn-secondary"></router-link>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            cards: [0,1,2,3]
+    import dates from '@/dates.json'
+
+    export default {
+        data() {
+            return {
+                dates
+            }
         }
     }
-}
 </script>
 
 <style scoped>
-    .card {
-        width: 15vw;
-        height: 20vh;
-        background-color: purple;
-    }
+
 </style>
