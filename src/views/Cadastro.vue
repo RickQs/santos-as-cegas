1ad6436a9f6b709b0b53294464fbd7b0627bedab
Henrique Rinaldi <henriquergr@hotmail.com>
2021-11-25 18:42:03 -0300

    refactor: novo botão e ajustes

--- src/views/Cadastro.vue
+++ src/views/Cadastro.vue
@@ -8,7 +8,7 @@
     </div>
 
     <section>
-        <form @submit="checkForm" action="/termo-consentimento">
+        <form @submit="checkRegisterForm" action="/termo-consentimento">
             <div v-if="errors.length" class="row mb-3">
                 <div class="col-12 col-md-12 col-lg-6">
                     <strong>Por favor, corrija o(s) seguinte(s) erro(s):</strong>
@@ -20,34 +20,45 @@
 
             <div class="row mb-3">
                 <div class="col-12 col-md-12 col-lg-6">
-                    <label for="exampleInputName" class="form-label">Nome</label>
-                    <input ref="nome" v-model="name" type="text" minlength="3" class="form-control border-success" id="exampleInputName" tabindex="1" aria-describedby="nameHelp">
+                    <label for="registerName" class="form-label">Nome</label>
+                    <input ref="nome" v-model="name" type="text" minlength="3" class="form-control border-success" id="registerName" tabindex="1" aria-describedby="nameHelp">
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col-12 col-md-12 col-lg-6">
-                    <label for="exampleInputAge" class="form-label">Data de nascimento</label><small> (dd/mm/yyyy)</small>
-                    <input ref="data de nascimento" v-model="birth" v-mask="'##/##/####'" type="tel" class="form-control border-success" id="exampleInputAge" tabindex="2" aria-describedby="ageHelp">
+                    <label for="registerBirth" class="form-label">Data de nascimento</label><small> (dd/mm/yyyy)</small>
+                    <input ref="data de nascimento" v-model="birth" v-mask="'##/##/####'" type="tel" class="form-control border-success" id="registerBirth" tabindex="2" aria-describedby="ageHelp">
                 </div>
             </div>
             <div class="row mb-3">
                 <div class="col-12 col-md-12 col-lg-6">
-                    <label for="exampleInputPhone" class="form-label">Telefone</label><small> ((00) 00000-0000)</small>
-                    <input ref="telefone" v-model="phone" v-mask="['(##) ####-####', '(##) #####-####']" type="tel" class="form-control border-success" id="exampleInputPhone" tabindex="3" aria-describedby="phoneHelp">
+                    <label for="registerPhone" class="form-label">Telefone</label><small> ((00) 00000-0000)</small>
+                    <input ref="telefone" v-model="phone" v-mask="['(##) ####-####', '(##) #####-####']" type="tel" class="form-control border-success" id="registerPhone" tabindex="3" aria-describedby="phoneHelp">
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-12 col-md-12 col-lg-6">
+                    <label for="registerEmail" class="form-label">Email</label>
+                    <input ref="email" v-model="email" type="email" class="form-control border-success" id="registerEmail" tabindex="4" aria-describedby="emailHelp">
                 </div>
             </div>
             <div class="row mb-5">
                 <div class="col-12 col-md-12 col-lg-6">
-                    <label for="exampleInputEmail" class="form-label">Email</label>
-                    <input ref="email" v-model="email" type="email" class="form-control border-success" id="exampleInputEmail" tabindex="4" aria-describedby="emailHelp">
+                    <label for="registerPassword" class="form-label">Senha</label>
+                    <input ref="senha" v-model="senha" type="password" class="form-control border-success" id="registerPassword" tabindex="5" aria-describedby="emailHelp">
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-6">
-                    <button class="btn btn-success w-100" role="button" tabindex="5" aria-disabled="false">
+                    <button class="btn btn-success w-100 mb-3" role="button" tabindex="6" aria-disabled="false">
                         Continuar
                     </button>
+                    <div class="text-center">
+                        <router-link to="views/login" class="link-secondary text-decoration-none" tabindex="7">
+                            Já tenho conta
+                        </router-link>
+                    </div>
                 </div>
             </div>
         </form>
@@ -62,15 +73,16 @@ export default {
             name: null,
             birth: null,
             phone: null,
-            email: null
+            email: null,
+            senha: null
         }
     },
     mounted() {
         this.$refs.nome.focus()
     },
     methods: {
-        checkForm(e) {
-            if (this.name && this.birth && this.birth.length === 10 && this.phone && this.phone.length >= 14 && this.email) {
+        checkRegisterForm(e) {
+            if (this.name && this.birth && this.birth.length === 10 && this.phone && this.phone.length >= 14 && this.email && this.senha) {
                 return true
             }
 
@@ -89,6 +101,9 @@ export default {
             }
             if (!this.email) {
                 this.errors.push('Email')
+            }
+            if (!this.password) {
+                this.errors.push('Senha')
             }
 
             let input = this.errors[0].toLowerCase()
